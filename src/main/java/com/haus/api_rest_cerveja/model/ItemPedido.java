4e3868228d439e0c0a3741016985a88f89e6f56f
Your Name <you@example.com>
2026-06-18 08:48:37 -0300

    Codigo do Pedido com DTO, Repository e Model completo

--- src/main/java/com/haus/api_rest_cerveja/model/ItemPedido.java
+++ src/main/java/com/haus/api_rest_cerveja/model/ItemPedido.java
@@ -1,4 +1,56 @@
 package com.haus.api_rest_cerveja.model;
 
+
+import jakarta.persistence.*;
+
+@Entity
 public class ItemPedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer quantidade;
+
+
+    @ManyToOne
+    @JoinColumn(name = "pedido_id")
+    private Pedido pedido;
+
+    @ManyToOne
+    @JoinColumn(name = "beer_id")
+    private Beer beer;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    public Beer getBeer() {
+        return beer;
+    }
+
+    public void setBeer(Beer beer) {
+        this.beer = beer;
+    }
 }
